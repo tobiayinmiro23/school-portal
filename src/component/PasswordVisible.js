65de94caee44628b415b1f2dad6de6b065251418
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-07-02 13:04:45 +0100

    Update PasswordVisible.js

--- src/component/PasswordVisible.js
+++ src/component/PasswordVisible.js
@@ -2,18 +2,18 @@ import React,{useState} from 'react'
 import '../styles/component/Password.scss'
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
-const PasswordVisible = ({Password,setPassword}) => {
+const PasswordVisible = ({Password,setPassword,placeholder}) => {
     const [Type, setType] = useState('password')
-  
+   
   return (
         <div className="password">
-            <input type={Type} value={Password} onChange={(e)=>setPassword(e.target.value)} placeholder='password' name="" id="" />
+            <input type={Type} value={Password} onChange={(e)=>setPassword(e.target.value)} placeholder={placeholder ? placeholder : 'password'}  name="" id="" />
             <div>
                 {
                   Type==='password' ?
-                  <div onClick={()=>setType('text')}><VisibilityOutlinedIcon className='eye'/></div>  
+                  <div onClick={()=>setType('text')}><VisibilityOutlinedIcon  className='eye'/></div>  
                   :
-                  <div onClick={()=>setType('password')}><VisibilityOffOutlinedIcon className='eye'/></div>
+                  <div onClick={()=>setType('password')}><VisibilityOffOutlinedIcon  className='eye'/></div>
                 }
             </div>
         </div>
