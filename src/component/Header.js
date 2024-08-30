7dff4f460f2d5f0cdab8d8fe39ef489f0a3b5f6e
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-08-30 20:42:11 +0100

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,33 +1,42 @@
-import React, {useContext } from 'react'
+import React, { useContext } from 'react'
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/component/Header.scss'
 import { userContext } from '../context/UserContext';
 
-function Header({setmobile,mobile}) {
-    let userData=useContext(userContext)
-    const [,,matricNumber,,,,,,username,]=userData
-    
+function Header({ setmobile, mobile }) {
+    const navigate = useNavigate()
+
+    let userData = useContext(userContext)
+    const [, , matricNumber, , , , , , username,] = userData
+
+    const deleteCookie = () => {
+        const expirationDate = new Date(0); // Set expiration date to the past
+        document.cookie = `creg=; expires=${expirationDate.toUTCString()}; path=/`;
+        document.cookie = `creg_img=; expires=${expirationDate.toUTCString()}; path=/`;
+        navigate('/Login')
+    };
+
     return (
         <nav className='navigation'>
             <div className="navwrapper">
-                    <div className="logo">
-                        <img src="/image/FUTMINNA_LOGO.png" alt="sch logo" />
-                    </div>
-                    <h1  className="schname">Futminna Portal</h1>
-                    <div className="name">
-                        <h3>{username}</h3>
-                        <p>{matricNumber}</p>
-                    </div>
-                    <div className="notification">
-                        <NotificationsIcon className="notificationImg"/>
-                        <div className="msg">0</div>
-                    </div>
-                    <div className='logout'><Link to='/login'>log out</Link></div>
-                    <div className="menu" onClick={()=>setmobile(!mobile)} >
-                        <MenuIcon/>
-                    </div> 
+                <div className="logo">
+                    <img src="/image/FUTMINNA_LOGO.png" alt="sch logo" />
+                </div>
+                <h1 className="schname">Futminna Portal</h1>
+                <div className="name">
+                    <h3>{username}</h3>
+                    <p>{matricNumber}</p>
+                </div>
+                <div className="notification">
+                    <NotificationsIcon className="notificationImg" />
+                    <div className="msg">0</div>
+                </div>
+                <div className='logout' onClick={deleteCookie}>log out</div>
+                <div className="menu" onClick={() => setmobile(!mobile)} >
+                    <MenuIcon />
+                </div>
             </div>
         </nav>
     )
