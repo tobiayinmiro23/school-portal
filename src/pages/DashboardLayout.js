8e48da0ad5ab9c06735b6c6ebe8bf9d73f1d94c1
Ayinmiro Tobi <ayinmirotobi@gmail.com>
2026-01-12 08:42:53 +0100

    Enhance DashboardLayout with scroll management
    Added scrollRef to manage scroll position on route changes.

--- src/pages/DashboardLayout.js
+++ src/pages/DashboardLayout.js
@@ -1,27 +1,34 @@
-import React,{useState,useContext,useEffect} from 'react'
-import { Outlet ,useNavigate} from 'react-router-dom'
-import { SideBar,Header } from '../AllFiles'
+import React, { useState, useContext, useEffect, useRef } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { SideBar, Header } from '../AllFiles'
 import '../styles/pages/DashboardLayout.scss'
+import { getPhoto } from '../hooks/Fetch'
+
 import { userContext } from '../context/UserContext';
 const DashboardLayout = () => {
   const [mobile, setmobile] = useState(false)
-  const navigate=useNavigate()
-  let userData=useContext(userContext)
-  const [,,,,,,,,username,]=userData
-  useEffect(()=>{
-    if(username==='')navigate('/')
-  })
+  const navigate = useNavigate()
+  let userData = useContext(userContext)
+  const [, , , , , , , , username,] = userData
+
+  const scrollRef = useRef(null)
+  const location = useLocation()
+  useEffect(async () => {
+    if (username === '') navigate('/')
+    scrollRef.current?.scrollTo(0, 0)
+
+  }, [location.pathname])
   return (
     <div className='layoutContainer'>
-        <Header setmobile={setmobile} mobile={mobile}/>
-        <div className='layout'>
-            <div className={mobile ? "sidebarMobile" : "sidebar"}>
-              <SideBar  setmobile={setmobile} mobile={mobile}/>
-            </div>
-            <div className='wrapper'>
-              <Outlet/>
-            </div>
+      <Header setmobile={setmobile} mobile={mobile} />
+      <div className='layout'>
+        <div className={mobile ? "sidebarMobile" : "sidebar"}>
+          <SideBar setmobile={setmobile} mobile={mobile} />
         </div>
+        <div className='wrapper' ref={scrollRef}>
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
